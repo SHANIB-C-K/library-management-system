bf3b2a7dee9fae8a2f031f89d4a735e1244c80c2
SHANIB-C-K <ckshanib8@gmail.com>
2026-03-14 19:51:04 +0530

    feat: Configure Vercel deployment and update MongoDB URI to use Atlas.

---
+++ api/index.py
@@ -0,0 +1,4 @@
+from app import app as application
+
+# This exports 'app' for Vercel's Python runtime
+app = application
--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'super-secret-key-for-dbms-lab'
-    MONGO_URI = os.environ.get('MONGO_URI') or 'mongodb://localhost:27017/'
+    MONGO_URI = os.environ.get('MONGO_URI') or 'mongodb+srv://shanib:shanib@cluster0.okcimp4.mongodb.net/'
     MONGO_DBNAME = 'library_db'
